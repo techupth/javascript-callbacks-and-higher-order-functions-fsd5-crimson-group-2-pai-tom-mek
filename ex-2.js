2116e6c3418137f32709812ead746faf35e86d54
Waramet Hatjantuek <waramet.fl@gmail.com>
2023-12-06 21:48:34 +0700

    feat: add completed exercise

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,15 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let scorePass = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] >= 70) {
+      scorePass.push(array[i]);
+    }
+  }
+  if (scorePass.length >= 5) {
+    operation(true);
+  } else console.log(scorePass);
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -13,3 +22,14 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+atLeastFive(studentScoresRoom1, function (score) {
+  scoreRoom1Result = score;
+});
+
+atLeastFive(studentScoresRoom2, function (score) {
+  scoreRoom1Result = score;
+});
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
